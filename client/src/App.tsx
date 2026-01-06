04d9c41f777263cce31cc3aa31bde3d3bd39d601
dhawalratnaparkhi <dhawal.ratnaparkhii@gmail.com>
2026-01-06 08:27:11 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,25 @@
-import { Switch, Route } from "wouter";
+import { Route, Switch } from "wouter";
 import Home from "@/pages/Home";
-import GroupTours from "@/pages/GroupTours";
-import CustomizedTour from "@/pages/CustomizedTour";
+import Tours from "@/pages/GroupTours";
+import CustomTour from "@/pages/CustomizedTour";
+import GroupTourDetails from "@/pages/GroupTourDetails";
 import Admin from "@/pages/Admin";
 
 export default function App() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route path="/tours" component={GroupTours} />
-      <Route path="/custom" component={CustomizedTour} />
+      <Route path="/tours" component={Tours} />
+      <Route path="/tours/:id" component={GroupTourDetails} />
+      <Route path="/custom" component={CustomTour} />
       <Route path="/admin" component={Admin} />
+
+      {/* Fallback */}
+      <Route>
+        <div style={{ padding: 40 }}>
+          <h1>404 – Page not found</h1>
+        </div>
+      </Route>
     </Switch>
   );
 }
